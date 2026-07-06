da4ed5333a23121dde95351d2ff17a7e9334a2f7
poojaryaryan <aryanpoojary152@gmail.com>
2026-07-06 16:29:11 +0530

    added play again and fixed too high/too low bug being printed even after attempts were 0

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -11,58 +11,90 @@ public class NumberGuessingGame {
         System.out.println("Number Guessing Game");
         System.out.println("********************");
 
-        System.out.println("Do you want to play with limited attempts or unlimited attempts?");
-        System.out.println("1. Limited Attempts");
-        System.out.println("2. Unlimited Attempts");
+        boolean playAgain = true;
 
-        System.out.print("Enter choice: ");
-        int choice = sc.nextInt();
+        while(playAgain) {
 
-        while (choice != 1 && choice != 2) {
-            System.out.print("Invalid choice! Enter 1 or 2: ");
-            choice = sc.nextInt();
-        }
+            System.out.println("Do you want to play with limited attempts or unlimited attempts?");
+            System.out.println("1. Limited Attempts");
+            System.out.println("2. Unlimited Attempts");
 
-        int attempts;
+            System.out.print("Enter choice: ");
+            int choice = sc.nextInt();
 
-        if (choice == 1) {
-            System.out.print("Enter the number of attempts: ");
-            attempts = sc.nextInt();
-        } else {
-            attempts = Integer.MAX_VALUE;
-        }
+            while (choice != 1 && choice != 2) {
+                System.out.print("Invalid choice! Enter 1 or 2: ");
+                choice = sc.nextInt();
+            }
 
-        int attemptsUsed = 0;
-        int number = random.nextInt(1, 101);
+            int attempts;
 
-        System.out.print("Guess a number from 1 to 100: ");
-        int guess = sc.nextInt();
+            if (choice == 1) {
+                System.out.print("Enter the number of attempts: ");
+                attempts = sc.nextInt();
+                while (attempts <= 0) {
+                    System.out.print("Attempts must be greater than 0. Enter again: ");
+                    attempts = sc.nextInt();
+                }
+            } else {
+                attempts = Integer.MAX_VALUE;
+            }
 
-        while (attempts > 0) {
+            int attemptsUsed = 0;
+            int number = random.nextInt(1, 101);
 
-            attemptsUsed++;
+            int guess = -1;
+
+            while (attempts > 0) {
+
+                System.out.print("Guess a number from 1 to 100: ");
+                guess = sc.nextInt();
+
+                attemptsUsed++;
+
+                if (guess == number) {
+                    break;
+                }
+
+                attempts--;
+
+                if(attempts > 0){
+                    if (guess > number) {
+                        System.out.println("Your guess is too high.");
+                    } else {
+                        System.out.println("Your guess is too low.");
+                    }
+                }
+            }
 
             if (guess == number) {
-                break;
-            }
-
-            if (guess > number) {
-                System.out.print("Your guess is too high. Try again: ");
+                System.out.println("\nYour guess is correct! The number was " + number);
+                System.out.println("Attempts used: " + attemptsUsed);
             } else {
-                System.out.print("Your guess is too low. Try again: ");
+                System.out.println("\nNo more attempts left!");
+                System.out.println("The correct number was " + number);
             }
 
-            attempts--;
-            guess = sc.nextInt();
+            System.out.println("\n==============================");
+            System.out.println("Would you like to play again?");
+            System.out.println("1. Yes");
+            System.out.println("2. No");
+            System.out.print("Enter choice: ");
+            int choice2 = sc.nextInt();
+
+            while (choice2 != 1 && choice2 != 2) {
+                System.out.print("Enter a VALID choice: ");
+                System.out.println("1. Yes");
+                System.out.println("2. No");
+                choice2 = sc.nextInt();
+            }
+
+            if(choice2==2){
+                playAgain = false;
+            }
         }
 
-        if (guess == number) {
-            System.out.println("\nYour guess is correct! The number was " + number);
-            System.out.println("Attempts used: " + attemptsUsed);
-        } else {
-            System.out.println("\nNo more attempts left!");
-            System.out.println("The correct number was " + number);
-        }
+        System.out.println("\nThanks for playing!");
 
         sc.close();
     }
